015d73fedfe5d18cd4806198dc017298a4e10bab
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-15 23:00:47 -0500

    really sussy baka

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const { Board, Led } = require("johnny-five");
-const board = new Board();
+const { Board, Led } = require('johnny-five');
 
-board.on("ready", () => {
-  // Create an Led on pin 13
-  const led = new Led(13);
-  // Blink every half second
-  led.blink(500);
+let myBoard,led,led2,led3;
+
+myBoard = new Board()
+
+myBoard.on('ready', function() {
+  led = new Led(13);
+  led.strobe(3000);
+
+  led2 = new Led(5);
+  led2.strobe(2000);
+
+  led3 = new Led(7);
+  led3.strobe(1000)
+
+});
+
+myBoard.on('error',function(err){
+  console.log(err);
 });
